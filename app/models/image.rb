114b72644f6488d0b8093ea38b0a19f4ef2864d9
Eliot Jordan <eliotj@princeton.edu>
2015-08-17 17:05:52 -0400

    changes to copyright behavior

--- app/models/image.rb
+++ app/models/image.rb
@@ -3,21 +3,12 @@ class Image < ActiveRecord::Base
   validates :bibid, :allow_blank => true, numericality: { only_integer: true,
     message:"not a valid bidid" }
   before_create :set_ark
+  after_update :update_ark
 
-  def set_ark
-    # base url for image viewer
-    target_base = ENV['EZID_TARGET_BASE']
-
-    # mint ark with EZID
-    identifier = Ezid::Identifier.create()
-
-    # set ARK target
-    noid = identifier.id.split('/')[-1]
-    identifier.target = target_base + noid
-    identifier.save
-
-    # set ark property to noid value
-    self.ark = noid
+  def default_desc
+    "This map was scanned with a Chroma Tx 40 Plus Wide Format scanner and " \
+    "WIDEimage 2.8 software at 400 DPI in 24-bit color. The image was then " \
+    "reduced to 256 colors."
   end
 
   def item_url
@@ -40,6 +31,40 @@ class Image < ActiveRecord::Base
         "#{search}")
     else
       all
+    end
+  end
+
+  def set_ark
+
+    # mint ark with EZID
+    identifier = Ezid::Identifier.create()
+
+    # set ARK target
+    noid = identifier.id.split('/')[-1]
+    identifier.target = self.target_base + noid
+    identifier.save
+
+    # set ark property to noid value
+    self.ark = noid
+  end
+
+  # updates ark target if copyright status changes
+  def update_ark
+    if self.copyright_changed?
+      full_id = ENV['EZID_DEFAULT_SHOULDER'][0..-4] + self.ark
+      identifier = Ezid::Identifier.find(full_id)
+      noid = identifier.id.split('/')[-1]
+      identifier.target = self.target_base + noid
+      identifier.save
+    end
+  end
+
+    # base target url for ark
+  def target_base
+    if self.copyright == false
+      ENV['EZID_TARGET_BASE']
+    else
+      "http://map.princeton.edu/offline/copyright.html#"
     end
   end
 end
